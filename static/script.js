3152e8c9576789cb190366574f830096ceaae0f8
Gökay Özcan <83103331+gokeay@users.noreply.github.com>
2023-09-12 13:48:27 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,23 +1,40 @@
-// HTML içindeki input alanına bir id ekleyin (örneğin, id="saatInput")
-// Bu id ile input alanını JavaScript'ten seçeceğiz.
+document.addEventListener("DOMContentLoaded", function() {
+    const form1 = document.getElementById("form1");
+    const form2 = document.getElementById("form2");
+    const form3 = document.getElementById("form3");
+    const form4 = document.getElementById("form4");
+    const gonderButton = document.getElementById("gonderButton");
 
-document.addEventListener('DOMContentLoaded', function() {
-    const saatInput = document.getElementById('saatInput');
-
-    saatInput.addEventListener('input', function() {
-        const saatDeğer = this.value;
-        if (saatDeğer.length === 2) {
-            this.value += '.';
+    form1.addEventListener("change", function() {
+        if (document.querySelector("select[name='yerBilgisi']").value) {
+            form2.style.display = "block";
+            // form1.style.display = "none";
         }
     });
+
+    form2.addEventListener("change", function() {
+        if (document.querySelector("select[name='durumBilgisi']").value) {
+            form3.style.display = "block";
+            // form2.style.display = "none";
+        }
+    });
+
+    form3.addEventListener("change", function() {
+        const fileInput = document.querySelector("input[type='file']");
+        if (fileInput && fileInput.files.length > 0) {
+            form4.style.display = "block";
+            // form3.style.display = "none";
+        }
+    });
+
+    form4.addEventListener("change", function() {
+        const numberInput = document.querySelector("input[type='number']");
+        if (numberInput && numberInput.value !== "") {
+            gonderButton.disabled = false;
+            gonderButton.classList.remove("pink-button");
+            gonderButton.classList.add("green-button");
+        }
+    });
+
+    
 });
-
-// function submitCrisis() {
-//     var crisisInfo = document.getElementById('crisis-info').value;
-//     var infoContainer = document.querySelector('.kriz-info');
-
-//     if (crisisInfo.trim() !== '') {
-//         infoContainer.innerHTML = '<p>' + crisisInfo + '</p>';
-//     }
-// }
-
