f1b8b00e3e39e8ee16a85c6d7b5d66c7dcf811cc
gokeay <ozcan301gokay@gmail.com>
2023-09-13 18:07:06 +0300

    updated

--- static/script.js
+++ static/script.js
@@ -19,6 +19,14 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
+    
+    form2.addEventListener("change", function() {
+        if (document.querySelector("select[name='durumBilgisi']").value == 'damacanaBilgisi') {
+            damacanaRow.style.display = 'flex';
+            damacanaAdet.style.display = 'block';
+        }
+    });
+
     form3.addEventListener("change", function() {
         const fileInput = document.querySelector("input[type='file']");
         if (fileInput && fileInput.files.length > 0) {
@@ -41,7 +49,45 @@ document.addEventListener("DOMContentLoaded", function() {
         // buraya verı akısıyle ılgılı kodlar yazılcaka
     });
 
+    document.getElementById('krizButton').addEventListener('click', function() {
+        document.getElementById('krizButton1').style.display = 'block';
+        document.getElementById('krizButton2').style.display = 'block';
+        document.getElementById('krizButton3').style.display = 'block';
+        // buraya verı akısıyle ılgılı kodlar yazılcaka
+    });
 
- 
+    document.getElementById('krizButton').addEventListener('click', function() {
+        document.querySelectorAll('.btn-group .btn').forEach(function(button) {
+            button.classList.remove('d-none');
+        });
+    });
+
+
+    document.querySelectorAll('.btn-group .btn').forEach(function(button) {
+        button.addEventListener('click', function() {
+            // Görünmez içeriği göster
+            document.getElementById('hiddenContent').classList.remove('d-none');
+        });
+    });
+    
+    // kriz bilgisini alır ve kriz içerigini yeniden görünmez yapar
+    document.getElementById('sendButton').addEventListener('click', function() {
+        // Burada yazılan mesajı alabilirsiniz
+        var message = document.getElementById('textBox').value;
+        console.log('Gönderilen Mesaj:', message);
+        // İleti gönderildikten sonra gizleme veya başka bir işlem yapma burada gerçekleştirilebilir
+        document.getElementById('hiddenContent').classList.add('d-none');
+        document.querySelectorAll('.btn-group .btn').forEach(function(button) {
+            button.classList.add('d-none'); });
+
+    });
+
+    document.getElementById('damacanaAdetInput').addEventListener('input', function() {
+        var adet = document.getElementById('damacanaAdetInput').value;
+        if (adet > 0) {
+            form3.style.display = "block";
+        }
+    });
+    
     
 });
